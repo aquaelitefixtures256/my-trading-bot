48d9a49229314e7c42f9ebf5d1e7b43425a06d06
Mwesigwa Faswihu <wilzuser@gmail.com>
2026-03-07 23:12:52 +0300

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'beast-black': '#060606',
+        'beast-mid': '#0b1220',
+        'beast-red': '#ff2d2d',
+        'beast-green': '#14ff8c'
+      }
+    }
+  },
+  plugins: [],
+};
